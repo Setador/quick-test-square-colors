730c5056535dbf53cde6e230e534831dc9dd2a46
Gabriel Macias <Setador@users.noreply.github.com>
2023-09-03 15:25:36 -0700

    Update index.js
    Update getRandomColor

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const drawContainer = (containerSize, childSize, numberOfChildren) => {
 
      // Function to generate a random color for children's background
     const getRandomColor = () => {
-        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     };
 
      // Array to store child elements
